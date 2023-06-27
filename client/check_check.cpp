2de1375a864520abfb60070230c37de5786ccf1d
Deependra Jadoun <deependra.singhjadoun@fysik.su.se>
2023-06-27 19:54:17 +0200

    Check working hopefully

--- client/check_check.cpp
+++ client/check_check.cpp
@@ -1,6 +1,8 @@
-namespace board {
+namespace board
+{
 
-	int CheckCheck(ChessBoard current_board) {
+	int CheckCheck(ChessBoard current_board)
+	{
 		// Checks if the current_board position is in check for color whose move it is
 		// Basically look at all off-color moves and see if any end at the square the current color king is on
 
@@ -9,28 +11,42 @@ namespace board {
 		vector<move::ChessMove> sq_moves;
 
 		char take_color;
-		if (current_board.color == 0) {
+		string color;
+		if (current_board.color == 0)
+		{
 			take_color = 'b';
-		} else {
+			color = "wK";
+		}
+		else
+		{
 			take_color = 'w';
+			color = "bK";
 		}
 
 		// Running over board to get king square and possible moves of other color
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
-				if (current_board.squares[i][j].substr(0,2) == current_board.color.append("K")) { // Checking king square
-					king_square = {i,j};
-					cout << "king at: " << i << "," << j << endl;
-				} else if (current_board.squares[i][j][0] == take_color) { // Else found a possible move square
-					sq_moves = GetPossibleMoves(current_board,i,j);
+		for (int i = 0; i < 8; i++)
+		{
+			for (int j = 0; j < 8; j++)
+			{
+				if (current_board.squares[i][j] == color)
+				{ // Checking king square
+					king_square[0] = i;
+					king_square[1] = j;
+					// cout << "king at: " << i << "," << j << endl;
+				}
+				else if (current_board.squares[i][j][0] == take_color)
+				{ // Else found a possible move square
+					sq_moves = move::GetPossibleMoves(current_board, i, j);
 					move_list.insert(move_list.end(), sq_moves.begin(), sq_moves.end());
 				}
 			}
 		}
 
 		// Seeing if any move ends on the king's square
-		for (m : move_list) {
-			if (m.end[0] == king_square[0] && m.end[1] == king_square[1]) {
+		for (move::ChessMove m : move_list)
+		{
+			if (m.end[0] == king_square[0] && m.end[1] == king_square[1])
+			{
 				return 1;
 			}
 		}
--- client_main.cpp
+++ client_main.cpp
@@ -16,6 +16,7 @@ using namespace std;
 #include "client/make_move.cpp"
 #include "client/print_board.cpp"
 #include "client/get_possible_moves.cpp"
+#include "client/check_check.cpp"
 #include "client/piece_reach.cpp"
 #include "client/parse_input.cpp"
 #include "client/player_move.cpp"
@@ -23,24 +24,28 @@ using namespace std;
 #include "client/generate_board.cpp"
 
 // Run program
-int main() {
+int main()
+{
 	// Display welcome message
 	cout << "Welcome to Chess++!" << endl;
 	cout << "Press Enter for a new game, otherwise input a path to a FEN file or the FEN sequence here." << endl;
 	string start_input;
 	// getline(cin, start_input);
-	start_input = "ch_fen.txt"; //// faster testing of FEN input, remove later
+	start_input = "ch_fen2.txt"; //// faster testing of FEN input, remove later
 	// start_input = ""; //// faster testing of blank input, remove later
 
 	board::ChessBoard current_board;
-	if (filesystem::exists(start_input)) { // Check if we input a file
+	if (filesystem::exists(start_input))
+	{ // Check if we input a file
 		cout << "FEN file \"" + start_input + "\" opened, importing FEN." << endl;
 		ifstream fen_file;
 		string fen_input;
 		fen_file.open(start_input);
-		getline(fen_file,fen_input);
+		getline(fen_file, fen_input);
 		current_board = board::GenerateBoard(fen_input);
-	} else { // No file, or doesn't exist
+	}
+	else
+	{ // No file, or doesn't exist
 		current_board = board::GenerateBoard("");
 	}
 
@@ -55,66 +60,100 @@ int main() {
 	// Check vs. engine or 2 player
 	cout << "Would you like to play vs. the engine (1) or a 2-player game (2)?" << endl;
 
-	while (cin) {
+	while (cin)
+	{
 		int play_mode;
 		cin >> play_mode;
-		if (play_mode == 1) { // vs. engine
+		if (play_mode == 1)
+		{ // vs. engine
 			cout << "Starting engine game..." << endl;
 			cout << "Which color would you like to play: w(hite), b(lack), r(andom)?" << endl;
-			cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
 			string player_color;
-			while (cin) {
+			while (cin)
+			{
 				cin >> player_color;
-				if (regex_match(player_color, regex("[Ww](?:[Hh](?:[Ii](?:[Tt](?:[Ee])?)?)?)?"))) {
-					cout << endl << "Player is white, engine is black." << endl;
+				if (regex_match(player_color, regex("[Ww](?:[Hh](?:[Ii](?:[Tt](?:[Ee])?)?)?)?")))
+				{
+					cout << endl
+						 << "Player is white, engine is black." << endl;
 					black_move = move::EngineMove;
 					break;
-				} else if (regex_match(player_color, regex("[Bb](?:[Ll](?:[Aa](?:[Cc](?:[Kk])?)?)?)?"))) {
-					cout << endl << "Engine is white, player is black." << endl;
+				}
+				else if (regex_match(player_color, regex("[Bb](?:[Ll](?:[Aa](?:[Cc](?:[Kk])?)?)?)?")))
+				{
+					cout << endl
+						 << "Engine is white, player is black." << endl;
 					white_move = move::EngineMove;
 					break;
-				} else if (regex_match(player_color, regex("[Rr](?:[Aa](?:[Nn](?:[Dd](?:[Oo](?:[Mm])?)?)?)?)?"))) {
+				}
+				else if (regex_match(player_color, regex("[Rr](?:[Aa](?:[Nn](?:[Dd](?:[Oo](?:[Mm])?)?)?)?)?")))
+				{
 					srand(time(0));
-					if (rand()/double(RAND_MAX) < 0.5) { // Player is white
-						cout << endl << "Player is white, engine is black." << endl;
+					if (rand() / double(RAND_MAX) < 0.5)
+					{ // Player is white
+						cout << endl
+							 << "Player is white, engine is black." << endl;
 						black_move = move::EngineMove;
-					} else { // Player is black
-						cout << endl << "Player is black, engine is white." << endl;
+					}
+					else
+					{ // Player is black
+						cout << endl
+							 << "Player is black, engine is white." << endl;
 						white_move = move::EngineMove;
 					}
 					break;
-				} else {
+				}
+				else
+				{
 					cout << "Invalid option!  Please choose a color to play: w(hite), b(lack), r(andom)?" << endl;
-					cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					cin.clear();
+					cin.ignore(numeric_limits<streamsize>::max(), '\n');
 				}
 			}
 			break;
-		} else if (play_mode == 2) { // 2-player
+		}
+		else if (play_mode == 2)
+		{ // 2-player
 			cout << "Starting 2-player game..." << endl;
 			break;
-		} else {
+		}
+		else
+		{
 			cout << "Invalid option!  Please choose to play vs. the engine (1) or a 2-player game (2)." << endl;
-			cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
 		}
 	}
 
 	clrscr();
 	// Main game loop
-	while (current_board.checkmate == 2) {
-		if (current_board.color == 0) { // White move
+	while (current_board.checkmate == 2)
+	{
+		if (current_board.color == 0)
+		{ // White move
 			current_board = white_move(current_board);
-		} else if (current_board.color == 1) { // Black move
+		}
+		else if (current_board.color == 1)
+		{ // Black move
 			current_board = black_move(current_board);
 		}
 	}
 
 	// Now game should be over (with mate or resign)
-	if (current_board.checkmate == 0) {
-		cout << "*** White wins! ***" << endl << endl;
-	} else if (current_board.checkmate == 1) {
-		cout << "*** Black wins! ***" << endl << endl;
+	if (current_board.checkmate == 0)
+	{
+		cout << "*** White wins! ***" << endl
+			 << endl;
 	}
-	cout << "Final position:" << endl << endl;
+	else if (current_board.checkmate == 1)
+	{
+		cout << "*** Black wins! ***" << endl
+			 << endl;
+	}
+	cout << "Final position:" << endl
+		 << endl;
 	board::PrintBoard(current_board);
 
 	return 0;
